7252bc5d279321f580423ec24c3f246bfd7aeafb
Frank Zhong <f.zhong@cascoda.com>
2021-07-16 11:33:41 +0100

    Improved client functionality (Getting IP address automatically; Option to run selected tests)

--- client_attempt.py
+++ client_attempt.py
@@ -17,6 +17,7 @@ __author__ = 'Giacomo Tanganelli'
 client = None
 
 def main():  # pragma: no cover
+    ## Get UUID ##
     host = "ff05::158"
     port = 5683
     path ="oic/res"
@@ -34,10 +35,28 @@ def main():  # pragma: no cover
     print("UUID is: " + response)
     client.stop()
 
-    subprocess.Popen(['C:\Program Files (x86)\OCF Conformance Test Tool\CTT_CLI.exe', '--enableAutomation', 
-                        '--apiEndpoint', 'http://192.168.12.60:32000', 
-                        '--oicserver', '--profile=Server', 
-                        '-pics=C:\Program Files (x86)\OCF Conformance Test Tool\PICS_module.json', 
+    ## Get IP address ##
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("1.1.1.1", 1))
+
+    ip_address = s.getsockname()[0]
+    print("IP address is: " + ip_address)
+    ip = 'http://' + ip_address + ':32000'
+
+    s.close()
+
+    ## Run OCF CTT ##
+    # Option 1: Run full test #
+    # subprocess.Popen([r'C:\Program Files (x86)\OCF Conformance Test Tool\CTT_CLI.exe', r'--enableAutomation', 
+    #                     r'--apiEndpoint', ip, 
+    #                     r'--oicserver', r'--profile=Server', 
+    #                     r'-pics=C:\Program Files (x86)\OCF Conformance Test Tool\PICS_module.json', 
+    #                     uuid])
+    
+    # Option 2: Run selected tests #
+    subprocess.Popen([r'C:\Program Files (x86)\OCF Conformance Test Tool\CTT_CLI.exe', 
+                        r'-s', r'--profile=C:\Users\frank\Documents\autoctt-main\OCF_profile.xml', 
+                        r'-pics=C:\Program Files (x86)\OCF Conformance Test Tool\PICS_module.json', 
                         uuid])
 
 if __name__ == '__main__':  # pragma: no cover
